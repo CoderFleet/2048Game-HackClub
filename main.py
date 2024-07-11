ab065ded216afe5f8a176f648cf30045ddbc5ee0
Rudransh Pratap Singh <87255169+CoderFleet@users.noreply.github.com>
2024-07-12 04:37:32 +0530

    Game over

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class Puzzle2048:
         self.add_random_tile()
         self.refresh_tiles()
         self.setup_controls()
+        self.game_over = False
 
     def create_tiles(self):
         self.tiles = []
@@ -27,16 +28,21 @@ class Puzzle2048:
             self.tiles.append(row)
 
     def add_random_tile(self):
-        empty_cells = [(x, y) for x in range(self.size) for y in range(self.size) if self.grid[x][y] == 0]
-        if empty_cells:
-            x, y = random.choice(empty_cells)
-            self.grid[x][y] = 2 if random.random() < 0.9 else 4
+        if not self.game_over:
+            empty_cells = [(x, y) for x in range(self.size) for y in range(self.size) if self.grid[x][y] == 0]
+            if empty_cells:
+                x, y = random.choice(empty_cells)
+                self.grid[x][y] = 2 if random.random() < 0.9 else 4
+            if self.check_game_over():
+                self.display_game_over()
 
     def refresh_tiles(self):
         for x in range(self.size):
             for y in range(self.size):
                 value = self.grid[x][y]
                 self.tiles[x][y].config(text=str(value) if value != 0 else "")
+        if self.check_game_over():
+            self.display_game_over()
 
     def setup_controls(self):
         self.window.bind("<Up>", self.go_up)
@@ -92,6 +98,39 @@ class Puzzle2048:
         if direction == 1:
             merged = merged[::-1]
         return merged
+
+    def check_game_over(self):
+        for x in range(self.size):
+            for y in range(self.size):
+                if self.grid[x][y] == 0:
+                    return False
+                if x > 0 and self.grid[x][y] == self.grid[x - 1][y]:
+                    return False
+                if x < self.size - 1 and self.grid[x][y] == self.grid[x + 1][y]:
+                    return False
+                if y > 0 and self.grid[x][y] == self.grid[x][y - 1]:
+                    return False
+                if y < self.size - 1 and self.grid[x][y] == self.grid[x][y + 1]:
+                    return False
+        return True
+
+    def display_game_over(self):
+        self.game_over = True
+        for x in range(self.size):
+            for y in range(self.size):
+                self.tiles[x][y].config(text="Game Over", bg="red", fg="white")
+        game_over_label = tk.Label(
+            self.window,
+            text="Game Over",
+            bg="black",
+            fg="white",
+            font=("Arial", 48)
+        )
+        game_over_label.grid(row=0, column=0, columnspan=self.size, rowspan=self.size)
+        self.window.unbind("<Up>")
+        self.window.unbind("<Down>")
+        self.window.unbind("<Left>")
+        self.window.unbind("<Right>")
 
 app = tk.Tk()
 game = Puzzle2048(app)
